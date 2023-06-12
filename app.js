e02ed26b5bb8741fbe7c1d93491e38411bb95cba
Vicentetw <perrottavicente@gmail.com>
2023-06-12 13:17:05 -0300

    uso de fs lee o crea archivo y crea carpeta package.json instalaciones sequalize sqlite

---
+++ app.js
@@ -0,0 +1,19 @@
+console.log("hola vicente");
+const fs = require('fs');
+
+fs.readFile("archivo1.txt", "utf8", (err, data) => {
+    if (err) throw err;
+
+    fs.writeFile("archivo2.txt", data, (err) => {
+        if (err) throw err;
+        console.log("El archivo ha sido guardado.");
+    });
+});
+fs.mkdir('results', (err) => {
+    if (err.code === 'EEXIST') {
+        console.error("Error: La carpeta ya está creada");
+    } else {
+        throw err;
+    }
+    return;
+});
