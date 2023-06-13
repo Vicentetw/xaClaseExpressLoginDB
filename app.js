45e9f390ce65205fcaa2ff9e4e1df1fefaf4f6b3
Vicentetw <perrottavicente@gmail.com>
2023-06-13 10:21:39 -0300

    I install express and create a function that listenson port 8080

--- app.js
+++ app.js
@@ -1,21 +1,9 @@
-console.log("hola vicente");
 
-const http = require('http');
 
-const server = http
-.createServer((req, res) =>{
-res.writeHead(200, {"Content-Type" : "text/html"});
+const express = require('express');
+const PORT = 8080;
+const app = express();
 
-console.log(`Req URL: ${req.url}`);
-if (req.url === "/hello"){
-    res.write ("<h1>Bienvenidos a mi sitio web hello !</h1>");
-    res.write ("<h2>Este es nuestro servidor!</h2>");
-} else{
-    res.write ("<h1>Hello word, Bienvenidos a mi sitio web</h1>");
-}
-
-res.end();
-})
-.listen(8080);
-//cerrar servidor
-//server.close(); 
+app.listen(PORT, () =>{
+    console.log( `hola vicente estoy escuchando en puerto: ${PORT}`);
+});
