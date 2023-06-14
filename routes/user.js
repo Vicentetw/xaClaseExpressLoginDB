9ce1d01509608d56a82c8c624088abfa14961267
Vicentetw <perrottavicente@gmail.com>
2023-06-14 14:29:59 -0300

    creo la ruta de user y la importo en app

--- routes/user.js
+++ routes/user.js
@@ -1,30 +1,32 @@
+const express = require("express");
+
 const router = express.Router();
 //reemplazo route. router.
-route.post("/user", (req, res) => {
+router.post("/user", (req, res) => {
 
     const { name, email, password } = req.body;
     res.send({ name, email, password: "****" });
 
 });
 
-route.get("/user/:userId", (req, res) => {
+router.get("/user/:userId", (req, res) => {
     const userId = req.params.userId;
     res.send({ userId });
 })
 
-route.get("/user", (req, res) => {
+router.get("/user", (req, res) => {
     const name = req.query.name;
     res.send({ name })
 });
 
 // cómo poner midware en una peticion route.put("/user/:userId", myMdw,(req, res) => {
-route.put("/user/:userId", (req, res) => {
+router.put("/user/:userId", (req, res) => {
     const userId = req.params.userId;
     const { name, email, password } = req.body;
     res.send({ id: userId, name, email, password: "****" });
 });
 
-route.delete("/user/:userId", (req, res) => {
+router.delete("/user/:userId", (req, res) => {
     const userId = req.params.userId;
     res.send(`Adios usuario ${userId}`);
 });
