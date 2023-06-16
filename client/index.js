bc3d95ba63e95340e036fb35b47ebfe9571e5edc
Vicentetw <perrottavicente@gmail.com>
2023-06-16 07:09:27 -0300

    agrego métodos PUT DELETE en client index.js

--- client/index.js
+++ client/index.js
@@ -1,3 +1,21 @@
+
+const axios = require("axios");
+const url = require("url");
+
+async function makeRequest() {
+let payload = { name: "John", email: "john@santex.com"};
+
+// Método PUT
+// const response = await axios.put("http://localhost:8080/user/1234",payload)
+const response = await axios.delete("http://localhost:8080/user/1234",payload)
+console.log("Datos del server: ", response.data);
+};
+
+makeRequest();
+
+
+/*metodo post*/
+/* 
 const axios = require("axios");
 const url = require("url");
 
@@ -9,6 +27,9 @@ console.log("Datos del server: ", response.data);
 };
 
 makeRequest();
+*/
+/*Fin post */
+
 /*metodo get*/
 /*const axios = require("axios");
 const url = require("url");//para caracteres especiales @
