b6a9f978a634a04bba29495d376cbfef56a4f542
Vicentetw <perrottavicente@gmail.com>
2023-06-26 07:58:52 -0300

    agrego validación par isbn dublicada en provider book

--- src/providers/bookProvider.js
+++ src/providers/bookProvider.js
@@ -1,6 +1,7 @@
 const {Op} = require("sequelize");
 const { Book } = require("../models");
 
+/*
 const createBook = async (bookOptions) => {
   try {
     const newBook = await Book.create(bookOptions);
@@ -10,7 +11,32 @@ const createBook = async (bookOptions) => {
     throw error;
   }
 };
-
+*/
+const createBook = async (bookOptions) => {
+    try {
+      const { isbn } = bookOptions;
+  
+      // Verificar si ya existe un libro con el mismo ISBN
+      const existingBook = await Book.findOne({
+        where: {
+          isbn: {
+            [Op.eq]: isbn, // Utilizando el operador de igualdad (eq)
+          },
+        },
+      });
+  
+      if (existingBook) {
+        throw new Error("ISBN already exists");
+      }
+  
+      // Crear el libro si no existe duplicado de ISBN
+      const newBook = await Book.create(bookOptions);
+      return newBook;
+    } catch (error) {
+      console.error("Error al crear book", error);
+      throw error;
+    }
+  };
 const deleteBook = async (bookId) => {
   try {
     const deletedBook = await Book.destroy({ where: { id: bookId } });
